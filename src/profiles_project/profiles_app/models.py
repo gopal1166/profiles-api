a96945fb5fa60d52d688ef9d3ba6beda5e301e57
pgopal1166 <pgopal1166@gmail.com>
2018-05-02 15:27:41 +0530

    Created a custom user profile model, a manager, and applied the migrations into database

--- src/profiles_project/profiles_app/models.py
+++ src/profiles_project/profiles_app/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our custom model"""
+
+    def create_user(self, email, name, password=None):
+        """Creats a new user profile object"""
+
+        if not email:
+            raise ValueError('Not a valid email')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creates and save a new superuser object"""
+
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Refers to user's profile """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    object = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """returns full name of the user"""
+
+        return self.name
+
+    def get_short_name(self):
+        """returns short name of user"""
+
+        return self.name
+
+    def __str__(self):
+        """returns a string which represents the object"""
+
+        return self.email

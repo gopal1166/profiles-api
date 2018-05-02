f8cbf7b15229b4014d6a81f402630e0ab31f8724
pgopal1166 <pgopal1166@gmail.com>
2018-05-03 05:23:09 +0530

    Created Viewset with list, create, retrieve, update, partial_update, destroy funcs

--- src/profiles_project/profiles_app/urls.py
+++ src/profiles_project/profiles_app/urls.py
@@ -1,7 +1,11 @@
-from . import views
-from django.conf.urls import url
+from .views import TestApiView, TestViewSet
+from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('test-viewset', TestViewSet, base_name='test_viewset')
+
 urlpatterns = [
-    url(r'^test/', views.TestApiView.as_view() )
+    url(r'^test/', TestApiView.as_view() ),
+    url(r'', include(router.urls) )
 ]
--- src/profiles_project/profiles_app/views.py
+++ src/profiles_project/profiles_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from django.http import HttpResponse
 from . import serializers
+from .serializers import TestSerializer
+
+from rest_framework import viewsets
 
 # Create your views here.
 class TestApiView(APIView):
@@ -40,3 +44,45 @@ class TestApiView(APIView):
         """Deletes an object """
 
         return Response({'status': 'success', 'method': 'delete'})
+
+
+class TestViewSet(viewsets.ViewSet):
+    """First ViewSet"""
+
+    def list(self, request):
+        """Returns a list """
+
+        myList = ['Gopa', 'Ram']
+        return Response({'method': 'list', 'list': myList})
+
+    def create(self, request):
+        """Creates an object """
+
+        serializer = TestSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Bello {0}'.format(name)
+            return Response({'status': 'success', 'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Retrives an object based on id """
+
+        return Response({'status': 'success', 'http_method': 'get'})
+
+    def update(self, request, pk=None):
+        """Updates an object based on id """
+
+        return Response({'status': 'success', 'http_method': 'put'})
+
+    def partial_update(self, request, pk=None):
+        """Partially updates an objec. only few fields"""
+
+        return Response({'status': 'success', 'http_method': 'patch'})
+
+    def destroy(self, request, pk=None):
+        """Deletes an object based on id"""
+
+        return Response({'status': 'success', 'http_method': 'delete'})
